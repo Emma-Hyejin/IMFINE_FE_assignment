91e93e1759d094846e25bfd3fcfccd10bede8976
Emma-Hyejin <cjd191058@naver.com>
2023-11-03 00:16:09 +0900

    Feat: Apply 버튼 작동 시 전체 데이터 수정 + renderTotl() 함수생성

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //js 파일
 //더미데이터
-const dummyDatas = [
+let dummyDatas = [
     {
         "id" : 0,
         "value" : 75,
@@ -22,75 +22,108 @@ const dummyDatas = [
         "value" : 70,
     }
 ];
+//최초 모든 함수 호출 
+function renderTotal () {
+    renderGraph();
+    renderTable();
+    // console.log(dummyDatas);
+};
 
-let yHeight = 14.5;
-let xList = document.getElementById('xList');
+renderTotal();
 
-dummyDatas.forEach( function(data) {
-    let xItem = document.createElement('li');
-    xItem.className = 'x_item';
+function renderGraph(){
+    const yHeight = 14.5;
+    const xList = document.getElementById('xList');
+    xList.innerHTML = '';
 
-    let xText = document.createElement('span');
-    xText.className = 'x_text';
-    xText.textContent = data.id;
-
-    let xBar = document.createElement('div');
-    xBar.className = 'x_bar';
-
-    let barItem = document.createElement('span');
-    barItem.className = 'bar_item';
-    barItem.style.height = (data.value / yHeight * 100) + '%';
-    // barItem.style.top = 100 - data.value + '%';
-
-    //xItem >x_text
-    //      >x_bar > bar_item
-    xBar.appendChild(barItem);
-    xItem.appendChild(xText);
-    xItem.appendChild(xBar);
-
-    xList.appendChild(xItem);
-});
+    dummyDatas.forEach( (data) => {
+        const xItem = document.createElement('li');
+        xItem.className = 'x_item';
+    
+        const xText = document.createElement('span');
+        xText.className = 'x_text';
+        xText.textContent = data.id;
+    
+        const xBar = document.createElement('div');
+        xBar.className = 'x_bar';
+    
+        const barItem = document.createElement('span');
+        barItem.className = 'bar_item';
+        barItem.style.height = (data.value / yHeight * 100) + '%';
+        // barItem.style.top = 100 - data.value + '%';
+    
+        //xItem >x_text
+        //      >x_bar > bar_item
+        xBar.appendChild(barItem);
+        xItem.appendChild(xText);
+        xItem.appendChild(xBar);
+    
+        xList.appendChild(xItem);
+    });
+};
 
 
 // 2. 값 변경 로직
-dummyDatas.forEach( (data) => {
-    let tbody = document.querySelector('.chart_table tbody');
-    let row = document.createElement('tr');
-    row.className = "chart_contents";
+function renderTable(){
+    const tbody = document.querySelector('.chart_table tbody');
+    tbody.innerHTML = '';
 
-    let idPart = document.createElement('td');
-    idPart.className = "chart_content id";
-    idPart.textContent = data.id;
-
-    let valuePart = document.createElement('td');
-    valuePart.className = "chart_content value";
-    valuePart.textContent = data.value;
-
-    let etcPart = document.createElement('td');
-    etcPart.className = "char_content etc";
+    dummyDatas.forEach( (data) => {
     
+        const row = document.createElement('tr');
+        row.className = "chart_contents";
+        row.setAttribute('data-id', data.id);
+    
+        const idPart = document.createElement('td');
+        idPart.className = "chart_content id";
+        idPart.textContent = data.id;
+    
+        const valuePart = document.createElement('td');
+        valuePart.className = "chart_content value";
+        valuePart.textContent = data.value;
+    
+        const etcPart = document.createElement('td');
+        etcPart.className = "char_content etc";
+        
+    
+        const deleteBtn = document.createElement('button');
+        deleteBtn.className = "deleteBtn";
+        deleteBtn.textContent = "삭제";
+    
+        etcPart.appendChild(deleteBtn);
+    
+        //row > idPart,valuePArt, etcPart
+        row.appendChild(idPart);
+        row.appendChild(valuePart);
+        row.appendChild(etcPart);
+    
+        tbody.appendChild(row);
+    }); 
 
-    let deleteBtn = document.createElement('button');
-    deleteBtn.className = "deleteBtn";
-    deleteBtn.textContent = "삭제";
+};
 
-    etcPart.appendChild(deleteBtn);
-
-    //row > idPart,valuePArt, etcPart
-    row.appendChild(idPart);
-    row.appendChild(valuePart);
-    row.appendChild(etcPart);
-
-    tbody.appendChild(row);
-}); 
 
 const deleteBtn = document.querySelectorAll('.deleteBtn');
+const applyBtn = document.querySelector('.applyBtn');
+const deleteArr = [];
 
 deleteBtn.forEach((button) => {
     button.addEventListener('click', function(){
         const row = button.closest('tr');
         if(row){
+            const id = parseInt(row.getAttribute('data-id'));
+            deleteArr.push(id);
             row.remove();
         }
     });
 });
+
+applyBtn.addEventListener('click', function(){
+    if(dummyDatas.length > 0 ){
+        dummyDatas = dummyDatas.filter(data => !deleteArr.includes(data.id));
+    } else {
+        console.error('데이터가 없습니다. ');
+    };
+
+    renderTotal();
+})
